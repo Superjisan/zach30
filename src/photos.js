834c37046fbce5fed8977f0e5ecbccbe179d462f
Jisan Zaman <jisan.zaman@dynamo.works>
2020-08-13 12:43:38 -0400

    first commit

---
+++ src/photos.js
@@ -0,0 +1,53 @@
+import * as classyAf from "./images/classy-af.jpg"
+import * as hotness from "./images/hotness.jpg"
+import * as laurapose from "./images/laurapose.jpg"
+import * as notWeird from "./images/not-weird.jpg"
+import * as pigeonDance from "./images/pigeondance.jpg"
+import * as poisonIvy from "./images/poison-ivy.jpg"
+import * as scheming from "./images/scheming.jpg"
+
+
+export const photos = [
+    {
+        src: classyAf,
+        width: 3,
+        height: 4,
+        title: "You are one classy lady Maggie! Love the umbrella"
+    },
+    {
+        src: hotness,
+        width: 4,
+        height: 4,
+        title: "Not sure what's really going on here, but I am going to interpret it as you find beauty whereever you go and that's majestic. Just like the muscular man here."
+    },
+    {
+        src: laurapose,
+        width: 5,
+        height: 4,
+        title: "Master of poses and hikes you are. So grateful for the amount of nature adventures you dragged me to."
+    },
+    {
+        src: notWeird,
+        width: 4,
+        height: 4,
+        title: "Never afraid to get weird, and that's what I love about you."
+    },
+    {
+        src: pigeonDance,
+        width: 3,
+        height: 4,
+        title: "There's no one I would rather see do a pigeon dance than you as an offering to the gods."
+    },
+    {
+        src: poisonIvy,
+        width: 3,
+        height: 4,
+        title: "Your halloween costumes have always been on fire, even since Bjork in high school. Your creativity, artistry, and drive to have the most original costume is something I admire in you."
+    },
+    {
+        src: scheming,
+        width: 5,
+        height: 4,
+        title: "You are always scheming for the next adventure. Nothing but respect for that."
+    },
+]

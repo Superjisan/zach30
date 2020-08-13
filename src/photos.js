36141b86f511b9c0a9b657077a7bc6e282338aa4
Jisan Zaman <jisan.zaman@dynamo.works>
2020-08-13 13:29:30 -0400

    added zach specific pics

--- src/photos.js
+++ src/photos.js
@@ -1,51 +1,73 @@
-import * as classyAf from "./images/classy-af.jpg"
-import * as hotness from "./images/hotness.jpg"
-import * as laurapose from "./images/laurapose.jpg"
-import * as notWeird from "./images/not-weird.jpg"
-import * as pigeonDance from "./images/pigeondance.jpg"
-import * as poisonIvy from "./images/poison-ivy.jpg"
-import * as scheming from "./images/scheming.jpg"
-
+import * as bachelorShoe from "./images/bachelor-shoe.jpg"
+import * as bad_hombres from "./images/bad_hombres.jpg"
+import * as buried_snow from "./images/buried_snow.jpg"
+import * as climbingCfa from "./images/climbing_cfa.jpg"
+import * as cute_couple from "./images/cute_couple.jpg"
+import * as dancer_extraordinaire from "./images/dancer_extraordinaire.jpg"
+import * as fancy_taiko from "./images/fancy_taiko.jpg";
+import * as grumpy_hat from "./images/grumpy_hat.jpg";
+import * as hanging_tree from "./images/hanging_tree.jpg";
+import * as hawaii_snorkling from "./images/hawaii_snorkling.jpg"
+import * as herb_shoulder from "./images/herb_shoulder.jpg"
+import * as hunky_bod from "./images/hunky_bod.jpg"
+import * as inaara_lion_queen from "./images/inaara_lion_queen.jpg"
+import * as jersey_boys from "./images/jersey_boys.jpg"
+import * as lorise from "./images/lorise.jpg"
+import * as naptime from "./images/naptime.jpg"
+import * as repping_bd from "./images/repping_bd.jpg"
+import * as retro from "./images/retro.jpg"
+import * as senior_cocks from "./images/senior_cocks.jpg"
+import * as serious_face from "./images/serious_face.jpg"
+import * as siblings from "./images/siblings.jpg"
+import * as soccer_sportsmanship from "./images/soccer_sportsmanship.jpg"
+import * as subway_face from "./images/subway_face.jpg"
+import * as twins from "./images/twins.jpg"
+import * as ultimate_hipster from "./images/ultimate_hipster.jpg"
+import * as ultimate_team from "./images/ultimate_team.jpg"
+import * as water_jump from "./images/water_jump.jpg"
+import * as weightlifting from "./images/weightlifting.jpg"
+import * as with_sister from "./images/with_sister.jpg"
+import * as young_guitar from "./images/young_guitar.jpg"
 
 export const photos = [
     {
-        src: classyAf,
+        src: bachelorShoe,
         width: 3,
         height: 4,
         title: "You are one classy lady Maggie! Love the umbrella"
     },
     {
-        src: hotness,
+        src: bad_hombres,
         width: 4,
         height: 4,
         title: "Not sure what's really going on here, but I am going to interpret it as you find beauty whereever you go and that's majestic. Just like the muscular man here."
     },
     {
-        src: laurapose,
+        src: buried_snow,
         width: 5,
         height: 4,
         title: "Master of poses and hikes you are. So grateful for the amount of nature adventures you dragged me to."
     },
     {
-        src: notWeird,
+        src: climbingCfa,
         width: 4,
         height: 4,
         title: "Never afraid to get weird, and that's what I love about you."
     },
     {
-        src: pigeonDance,
+        src: cute_couple,
         width: 3,
         height: 4,
         title: "There's no one I would rather see do a pigeon dance than you as an offering to the gods."
     },
     {
-        src: poisonIvy,
+        src: dancer_extraordinaire,
         width: 3,
         height: 4,
         title: "Your halloween costumes have always been on fire, even since Bjork in high school. Your creativity, artistry, and drive to have the most original costume is something I admire in you."
     },
     {
-        src: scheming,
+        src: fancy_taiko,
         width: 5,
         height: 4,
         title: "You are always scheming for the next adventure. Nothing but respect for that."
